891bc95996ee6030fd77b8eead6ea9db829c1cc7
huythai <thaiquanghuy.c3a@gmail.com>
2025-03-13 15:03:49 +0700

    style: minor css improvement

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,7 +5,25 @@ import { providePrimeNG } from 'primeng/config';
 import Material from '@primeng/themes/material';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { definePreset } from '@primeng/themes';
 
+const MyPreset = definePreset(Material, {
+  semantic: {
+    primary: {
+      50: '{blue.50}',
+      100: '{blue.100}',
+      200: '{blue.200}',
+      300: '{blue.300}',
+      400: '{blue.400}',
+      500: '{blue.500}',
+      600: '{blue.600}',
+      700: '{blue.700}',
+      800: '{blue.800}',
+      900: '{blue.900}',
+      950: '{blue.950}',
+    },
+  },
+});
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
@@ -15,7 +33,7 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({
       ripple: true,
       theme: {
-        preset: Material,
+        preset: MyPreset,
         options: {
           darkModeSelector: false || 'none',
         },
