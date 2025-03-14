15ff111b27c461a8055cb61117f8266a74078176
huythai <thaiquanghuy.c3a@gmail.com>
2025-03-14 19:12:41 +0700

    style: flex wrapper for the header to improve ui on small screen

--- frontend/src/app/components/toolbar/toolbar.component.ts
+++ frontend/src/app/components/toolbar/toolbar.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../shared/shared/shared.module';
-import { FileUploadComponent } from './file-upload/file-upload.component';
 import { PlaybackControlsComponent } from './playback-controls/playback-controls.component';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [SharedModule, FileUploadComponent, PlaybackControlsComponent],
+  imports: [SharedModule, PlaybackControlsComponent],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
-export class ToolbarComponent {}
+export class ToolbarComponent { }
